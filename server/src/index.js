905f631bfcb475f96a28d1ab93ecd2ecd69e5ce5
Efosa-3012 <57777213+Efosa-3012@users.noreply.github.com>
2023-05-07 23:59:47 -0400

    o-o

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 import { usersRouter } from "./routes/users.js";
 import { booksRouter } from "./routes/books.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -13,10 +16,10 @@ app.use(cors());
 app.use("/auth", usersRouter);
 app.use("/books", booksRouter);
 
-mongoose.connect(
-  "mongodb+srv://efisu1:Pochita30.12@library.znhfo0t.mongodb.net/library?retryWrites=true&w=majority"
-);
+mongoose.connect(process.env.MONGODB_URI);
 
-app.listen(3001, () => {
-  console.log("Server started at port 3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
 });
